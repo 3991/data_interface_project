35cbfe43b36b33e5cae6a4d25bb64208543c4377
3991 <cardon.aristide@live.fr>
2016-10-23 00:28:34 +0200

    Delete bad informations about URL

--- test.php
+++ test.php
@@ -48,7 +48,7 @@
 </div>
 <div class="container">
 <?php
-echo file_get_contents("http://expnoce.lifl.fr:8000/api/1.0/tweets/get?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6ImFyaXN0aWRlLmNhcmRvbkBnbWFpbC5jb20iLCJzYWx0IjoidXNlclNhbHQiLCJsZXZlbCI6ImJhc2ljIiwiaWF0IjoxNDc2OTg5Njg3fQ.dl-vepsrgC6NAllLXR5qXkxh_ucjrxx53PY89dB9QZA&language=fr&expression=lille");
+echo file_get_contents("");
 ?> 
 </div>
 </body>
