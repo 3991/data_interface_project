0d253643ebfb85638b3edd742e57d35af4614340
3991 <cardon.aristide@live.fr>
2016-10-23 00:19:41 +0200

    Test the API

---
+++ test.php
@@ -0,0 +1,5 @@
+
+<?php
+echo file_get_contents("http://expnoce.lifl.fr:8000/api/1.0/tweets/get?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6ImFyaXN0aWRlLmNhcmRvbkBnbWFpbC5jb20iLCJzYWx0IjoidXNlclNhbHQiLCJsZXZlbCI6ImJhc2ljIiwiaWF0IjoxNDc2OTg5Njg3fQ.dl-vepsrgC6NAllLXR5qXkxh_ucjrxx53PY89dB9QZA&language=fr&expression=lille");
+?> 
+                               		
